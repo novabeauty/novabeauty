7f548f37f74da1766e9600be0cc7d4f02daebd6c
asadovnikov <rn.cruger@gmail.com>
2017-04-10 18:00:53 +0300

    v 0.1

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -14,6 +14,13 @@
     vm.creationDate = 1489692822655;
     vm.showToastr = showToastr;
     vm.scrollToSection = scrollTo;
+    vm.openMap = openMap;
+
+    function openMap(){
+      var url = 'https://yandex.ua/maps/143/kyiv/?ll=30.536315%2C50.414448&z=17&mode=search&text=%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0%2C%20%D0%9A%D0%B8%D0%B5%D0%B2%2C%20%D0%B1%D1%83%D0%BB%D1%8C%D0%B2%D0%B0%D1%80%20%D0%94%D1%80%D1%83%D0%B6%D0%B1%D1%8B%20%D0%9D%D0%B0%D1%80%D0%BE%D0%B4%D0%BE%D0%B2%2013&sll=30.536315%2C50.414448&sspn=0.032315%2C0.012066';
+      var win = window.open(url, '_blank');
+      win.focus();
+    }
 
     function scrollTo(anchor){
       $(anchor).animatescroll();
