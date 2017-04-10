e7be82cc569f901da344a52c0cd2809fe808496b
asadovnikov <rn.cruger@gmail.com>
2017-04-10 15:44:58 +0300

    publish: v 0.1
    generated from commit 38c7ce8d535b05f2de5a3f1019f68eb55c14687d

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('novabeauty', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr']);
-
-})();
